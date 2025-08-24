f0fcc19bd47bb0187689b3a274104a14e3affc3e
Sanjay Chaturvedi <sanjaychaturvedi36@gmail.com>
2025-08-24 18:56:50 +0530

    Add the source file for pointer_1 example.

---
+++ pointer_1.c
@@ -0,0 +1,31 @@
+/* program to get familiar with pointer syntax: decleration, assignment, access */
+
+#include<stdio.h>
+
+int main()
+{
+    int x = 40;     // integer type variable
+
+    int* px;        // decleration of pointer to variable x 
+    px = &x;        // assignment of variable address to pointer
+
+    int** ppx;      // pointer to above declared pointer px
+    ppx = &px;      // assigning address of px    
+
+    int*** pppx;    // pointer to above declared pointer ppx
+    pppx = &ppx;    // assigning address of ppx
+
+    printf("Vaule assigned to x : %d\n",x);
+    printf("Address in pointer px   : %p, value at address : %d\n",px,*px);     // access value of x through pointer px
+    printf("Address in pointer ppx  : %p, value at address : %p\n",ppx,*ppx);   // access value of ppx
+    printf("Address in pointer pppx : %p, value at address : %p\n",pppx,*pppx); // acess value of pppx
+    printf("\n");
+
+    // additional lines of code for curious minds
+    printf("Some experiments with a pointer(pppx) to a pointer(ppx):\n");
+    printf("Address in pointer pppx : %p, value at address : %p\n",pppx,*pppx); // pointer format specifier
+    printf("Address in pointer pppx : %p, value at address : %X\n",pppx,*pppx); // Hexadecimal format specifier
+    printf("Address in pointer pppx : %p, value at address : %d\n",pppx,*pppx); // integer format specifier
+
+    return 0;
+}
