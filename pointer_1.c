084bd2d8b80e07ec46a003db7128e2b1cb4ae1c7
Sanjay Chaturvedi <sanjaychaturvedi36@gmail.com>
2025-08-25 07:50:29 +0530

    Example 1: pointer syntax(declare, assign, access)

--- pointer_1.c
+++ pointer_1.c
@@ -18,13 +18,13 @@ int main()
     printf("Vaule assigned to x : %d\n",x);
     printf("Address in pointer px   : %p, value at address : %d\n",px,*px);     // access value of x through pointer px
     printf("Address in pointer ppx  : %p, value at address : %p\n",ppx,*ppx);   // access value of ppx
-    printf("Address in pointer pppx : %p, value at address : %p\n",pppx,*pppx); // acess value of pppx
+    printf("Address in pointer pppx : %p, value at address : %p\n",pppx,*pppx); // access value of pppx
     printf("\n");
 
     // additional lines of code for curious minds
     printf("Some experiments with a pointer(pppx) to a pointer(ppx):\n");
     printf("Address in pointer pppx : %p, value at address : %p\n",pppx,*pppx); // pointer format specifier
-    printf("Address in pointer pppx : %p, value at address : %X\n",pppx,*pppx); // Hexadecimal format specifier
+    printf("Address in pointer pppx : %p, value at address : %X\n",pppx,*pppx); // hexadecimal format specifier
     printf("Address in pointer pppx : %p, value at address : %d\n",pppx,*pppx); // integer format specifier
 
     return 0;
