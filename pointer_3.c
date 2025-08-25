eea6c0666f80b8287da968c90fb5244a7098f857
Sanjay Chaturvedi <sanjaychaturvedi36@gmail.com>
2025-08-25 17:04:13 +0530

    Example 3: pointer to update values via function

---
+++ pointer_3.c
@@ -0,0 +1,46 @@
+/*
+Program to show the use of a pointer as an argument to a function for updating a variable
+defined in the main function.
+-----------------------------
+In this example, total number of available books is updated after new books come in donation.
+The number of donated books is fed by the user and the program updates the total number of
+books by calling a function.
+*/
+
+#include <stdio.h>
+
+// function protoype
+void updateBooksCount(int, int *);
+
+// main function
+int main()
+{
+    int donated_books = 0;
+    int total_books = 0;
+
+    // pointer to total books
+    int *ptotal_books;           // pointer decleration
+    ptotal_books = &total_books; // assignment
+
+    for (int i = 0; i < 5; i++) // five iterations
+    {
+        // promt user to input the number of books
+        printf("Enter the number of books donates: ");
+        scanf("%d", &donated_books);
+
+        // call the function to update books count
+        updateBooksCount(donated_books, ptotal_books);
+
+        // total number of books
+        printf("Total books collected as of now: %d\n", total_books);
+    }
+}
+
+// function defintion
+// (Important: variable names are same but variables are different)
+void updateBooksCount(int donated_books, int *ptotal_books)
+{
+    int temp = *ptotal_books; // dereferencing pointer
+    temp = temp + donated_books;
+    *ptotal_books = temp; // updating the value stored at location pointed by pointer y
+}
