f183068c314a8178efefcfa012494f17e1619eaa
sanjay <sanjaychaturvedi36@gmail.com>
2025-09-04 16:55:45 +0530

    Added "out of bound" failure example

--- arrays_1.c
+++ arrays_1.c
@@ -7,15 +7,30 @@ int main()
     /*int arr1[5];
     arr1 = {1,2,3,4,5}; */
 
+    /*======================================*/
     // Array declaration and array intialization of individual element
     /*int arr1[5];
     arr1[0] = 1; arr1[1] = 2; arr1[2] = 3; arr1[3] = 4; arr1[4] = 5;*/ 
 
+    /*======================================*/
     // Array intialization with curly braces at the time of declaration(with array size)
     /*int arr1[5] = {1,2,3,4,5};*/
 
+    /*======================================*/
     // Array intialization with curly braces at the time of declaration(without array size)
-    int arr1[] = {1,2,3,4,5};
+    /*int arr1[] = {1,2,3,4,5};*/
+
+    /*======================================*/
+    // An array of name arr1 with one element is initalized with zero
+    // If elements beyond the array size are accessed, then C language 
+    // does not give any out of bound error and prints the value present
+    // in the pointed loactions.
+    /*int arr1[] = {0};*/
+
+    /*======================================*/
+    // Array of five elements and all initialized to zero
+    int arr1[5] = {0};
+
 
     for(int i=0; i<5;i++)
     {
