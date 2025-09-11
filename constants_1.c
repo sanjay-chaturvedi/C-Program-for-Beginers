6ab816f922b03ebc80cf1f4c156dace6bd5ac893
sanjay <sanjaychaturvedi36@gmail.com>
2025-09-11 13:15:23 +0530

    Example: using scanf and buffer related problems

--- constants_1.c
+++ constants_1.c
@@ -1,4 +1,7 @@
-/* program declaring constants */
+/* program to take inputs from the user and the problems with scanf() */
+/* seprate the format specifiers in the scanf function by space, tab or newline */
+/* for scanf() to know that user has given the input, press enter/return key */
+/* issue with garbage value in the input buffer when calling scanf() */
 
 #include<stdio.h>
 
@@ -9,25 +12,37 @@ int main()
     float c2 = 10.10;
     char c3 = 's';
 
-    printf("Constant c1, c2 and c3 are: %d, %.2f and %c.\n",c1,c2,c3);
+    printf("Variables c1, c2 and c3 are: %d, %.2f and %c.\n",c1,c2,c3);
 
-    // taking input
+    // taking input: no space between the format specifiers lead to incorrect reading of inputs (%d%f%c)
+    // In this call to scanf function , when I enter values <1010.10s>, the output I get is <1010 0.10 s>,
+    // when I enter values <10 10.10 s>, the output I get is <10 10.10 empty>, additionally in the latter case
+    // I do not get any prompt to enter input for the second scanf() call. This is due to garbage value already present in the buffer.
     int c4;
     float c5;
     char c6;
 
     printf("Eneter an integer, real and character value: ");
     scanf("%d%f%c",&c4,&c5,&c6);
-    printf("Constant c4, c5 and c6 are: %d, %.2f and %c.\n",c4,c5,c6);
+    printf("Variables c4, c5 and c6 are: %d, %.2f and %c.\n",c4,c5,c6);
 
-    // taking input
+    // cleaning the input buffer after the previous call to scanf()
+    // In this call to scanf function , when I enter values <10 10.10 s>, the output I get is <10 10.10 s>,
+    // when I enter values <1010.10s>, the output I get is <1010 0.10 s>, so enter value with a space sepration.
+
+    int test;
+    while((test = getchar()) != '\n' && test != EOF);
+    
+
+    // taking input: space between the format specifiers solves the above problem (%d %f %c)
     int c7;
     float c8;
     char c9;
 
     printf("Eneter an integer, real and character value: ");
-    scanf("%d%f%c", &c7, &c8, &c9);
-    printf("Constant c7, c8 and c9 are: %d, %.2f and %c.\n",c7,c8,c9);
+    scanf("%d %f %c", &c7, &c8, &c9);
+    printf("Variables c7, c8 and c9 are: %d, %.2f and %c.\n",c7,c8,c9);
+    
 
     return 0;
 }
