1517d6aa13531bff3768b5fd2bf6b5aba9a4f8cd
sanjay <sanjaychaturvedi36@gmail.com>
2025-09-13 15:38:58 +0530

    Example: added function int_power and num_of_digits

--- math_2.c
+++ math_2.c
@@ -5,35 +5,52 @@
  - sum of all digits */
 
  #include<stdio.h>
- #include<math.h>
 
- // function prototype
- int num_of_digits(int*);
+ // function prototype, taking a pointer and returning a pointer
+ int* num_of_digits(int*);
+ int int_power(int,int);
+
 
  // function main
  int main(void)
  {
-   int num;
+   int num = 0;
    printf("Enter the number: ");
    scanf("%d",&num);
 
-   int* pnum = &num;
-   int digit_count;
-   digit_count = num_of_digits(pnum);
-   printf("Number of digits: %d",digit_count);
+   printf("Number of digits: %d\n",*num_of_digits(&num));
+
+   int base = 0, power = 0;
+   printf("Enter the base number and the exponent:");
+   scanf("%d %d",&base,&power);
+   printf("Output of int_power function is: %d\n",int_power(base,power));
 
    return 0;
  }
 
  // function definition
- int num_of_digits(int* pnum)
+ int* num_of_digits(int* pnum)
  {
-    int num, digit_count;
-    num = *pnum; 
+    int num = *pnum;
+    static int digit_count = 0;
 
-    num/10
+    while (num != 0)
+    {
+      num = num/10;
+      digit_count++;
+    }
+    
+    return &digit_count;
+ }
 
+ // function power
+ int int_power(int base, int exp)
+ {
+    int out = base;
+    for(int i = 2; i <= exp; i++)
+    {
+      out = out*base;
+    }
 
-
-    return digit_count;
+    return out;
  }
